fc2cfc28f84025fc915dd1a48f753eb314c65098
bahrom <bahromprimqulov2003@gmail.com>
2024-01-17 18:51:01 +0500

    second commit

---
+++ app/src/main/java/uz/bahrom/note/app/MyApp.kt
@@ -0,0 +1,8 @@
+package uz.bahrom.note.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApp : Application() {
+}
---
+++ app/src/main/java/uz/bahrom/note/ui/UpdateNoteActivity.kt
@@ -0,0 +1,12 @@
+package uz.bahrom.note.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uz.bahrom.note.R
+
+class UpdateNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_note)
+    }
+}
